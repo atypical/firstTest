cf5148d0c895cf5bd708cfb53ad48e313689db58
MDavid Low <mdavidlow@gmail.com>
2020-09-21 10:30:33 -0400

    Initial Commit

---
+++ firstTest/firstTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTestApp.swift
+//  firstTest
+//
+//  Created by MDavid Low on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct firstTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
